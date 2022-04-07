e6a68f9615b825b8491ec4b3115a8e0e4109db1c
Estelle <estelle.wu117@gmail.com>
2022-04-07 12:18:04 +1200

    rearrange api file

--- src/components/api.js
+++ src/components/api.js
@@ -2,8 +2,8 @@ const baseurl='https://travel-functionapp.azurewebsites.net/api/'
 
 // GET/ all users
 export async function allUsers(){
-    const response=await fetch(`${baseurl}users`)
-    return response.json()
+    const res=await fetch(`${baseurl}users`)
+    return res.json()
 }
 // GET/ user/{id}
 export async function getUserId(id){
@@ -11,20 +11,20 @@ export async function getUserId(id){
     return res.json()
 }
 
+// GET/ search user/{name}
+export async function searchUser(name){
+    const res=await fetch(`${baseurl}name/${name}`)
+    return res.json()
+}
+
 // POST/ user
 export async function postUser(data){
-    const response=await fetch(`${baseurl}postuser`, {
+    const res=await fetch(`${baseurl}postuser`, {
         method:'POST',
         headers:{ 'Content-Type': 'application/json' },
         body:JSON.stringify(data)
     })
-    return response.ok
-}
-
-// GET/ search user by name
-export async function searchUser(name){
-    const res=await fetch(`${baseurl}name/${name}`)
-    return res.json()
+    return res.ok
 }
 
 //DELETE/ user/{id}
@@ -49,4 +49,5 @@ export async function changeUser(id,data){
            },
         body: JSON.stringify(data)
     })
+    return res.ok
 }
